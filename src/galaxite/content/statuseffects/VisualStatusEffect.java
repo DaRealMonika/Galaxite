0aaf09fac906a41de6ab488b5711f1d303af550e
amand <amandagraham232@gmail.com>
2023-05-04 20:29:08 -0400

    fix(?)

--- src/galaxite/content/statuseffects/VisualStatusEffect.java
+++ src/galaxite/content/statuseffects/VisualStatusEffect.java
@@ -13,17 +13,21 @@ public class VisualStatusEffect extends StatusEffect {
     public static String sprite;
     public static Float rotateSpeed;
     public static Color spriteColor;
+    private static TextureRegion Sprite;
 
     public VisualStatusEffect(String name){
         super(name);
     }
 
     @Override
-    public void update(Unit unit, float time){
-        super.update(unit, time);
-        var Sprite = new TextureRegion();
-        if (Icon.icons.get(sprite) != null) Sprite = Icon.icons.get(sprite).getRegion();
-        else Sprite = Core.atlas.find(sprite);
+    public void load() {
+        super.load();
+        Sprite = Icon.icons.get(sprite) != null ? Icon.icons.get(sprite).getRegion() : Core.atlas.find(sprite);
+    }
+
+    @Override
+    public void draw(Unit unit, float time){
+        super.draw(unit, time);
 
         if (spriteColor != null) Draw.color(spriteColor);
         Draw.rect(Sprite, unit.x + Angles.trnsx(unit.rotation, 0, 0), unit.y + Angles.trnsy(unit.rotation, 0, 0), Time.time * rotateSpeed);
